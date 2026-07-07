181d17f2ac393719d133e1efdb9eb9c562f68079
sashik mindaka <mindakaofficial0069@gmail.com>
2026-07-07 17:23:38 +0530

    add courseService

---
+++ backend/universityManagement/src/main/java/com/example/universityManagement/service/CourseService.java
@@ -0,0 +1,26 @@
+package com.example.universityManagement.service;
+
+import com.example.universityManagement.model.Course;
+import com.example.universityManagement.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Course saveCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public void deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+    }
+}
---
+++ backend/universityManagement/src/main/java/com/example/universityManagement/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.example.universityManagement.service;
+
+import com.example.universityManagement.model.Employee;
+import com.example.universityManagement.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
