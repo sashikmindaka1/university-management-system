b5d282201e361d31143e71c4f47b7e4a1c495a9d
sashik mindaka <mindakaofficial0069@gmail.com>
2026-07-07 16:46:30 +0530

    attendence code add

--- backend/universityManagement/src/main/java/com/example/universityManagement/model/Course.java
+++ backend/universityManagement/src/main/java/com/example/universityManagement/model/Course.java
@@ -1,4 +1,18 @@
 package com.example.universityManagement.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "courses")
+@Data
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String courseCode;
+    private String courseName;
+    private int credits;
 }
